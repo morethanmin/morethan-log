5cc8fbc19f0368d82128d3e57377d7ac9ea572cb
frontend-sangmin <lee.sangmin@cashwalk.io>
2022-06-27 14:38:27 +0900

    :mag: sitemap 변경

--- src/pages/sitemap.xml.js
+++ src/pages/sitemap.xml.js
@@ -16,27 +16,27 @@ export const getServerSideProps = async ({ res }) => {
   const allPaths = [...dynamicPaths]
 
   const sitemap = `
-    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:news="http://www.google.com/schemas/sitemap-news/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml" xmlns:mobile="http://www.google.com/schemas/sitemap-mobile/1.0" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1" xmlns:video="http://www.google.com/schemas/sitemap-video/1.1">
-    <url>
-      <loc>${link}</loc>
-      <lastmod>${new Date().toISOString()}</lastmod>
-      <changefreq>monthly</changefreq>
-      <priority>1.0</priority>
-    </url>
-    <url>
-      <loc>${link}/feed</loc>
-      <lastmod>${new Date().toISOString()}</lastmod>
-      <changefreq>monthly</changefreq>
-      <priority>1.0</priority>
-    </url>
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:news="http://www.google.com/schemas/sitemap-news/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml" xmlns:mobile="http://www.google.com/schemas/sitemap-mobile/0.7" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1" xmlns:video="http://www.google.com/schemas/sitemap-video/1.1">
+      <url>
+        <loc>${link}</loc>
+        <changefreq>daily</changefreq>
+        <priority>0.7</priority>
+        <lastmod>${new Date().toISOString()}</lastmod>
+      </url>
+      <url>
+        <loc>${link}/feed</loc>
+        <changefreq>daily</changefreq>
+        <priority>0.7</priority>
+        <lastmod>${new Date().toISOString()}</lastmod>
+      </url>
     ${allPaths
       .map((url) => {
         return `
           <url>
             <loc>${url}</loc>
+            <changefreq>daily</changefreq>
+            <priority>0.7</priority>
             <lastmod>${new Date().toISOString()}</lastmod>
-            <changefreq>monthly</changefreq>
-            <priority>1.0</priority>
           </url>
         `
       })
