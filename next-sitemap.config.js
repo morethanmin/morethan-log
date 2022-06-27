a6e5d24122dac3df96f0bdc625549ab3c4dab33d
frontend-sangmin <lee.sangmin@cashwalk.io>
2022-06-27 12:18:38 +0900

    :bug: build error

--- next-sitemap.config.js
+++
@@ -1,9 +0,0 @@
-const CONFIG = require('./blog.config')
-
-module.exports = {
-  siteUrl: CONFIG.link,
-  generateRobotsTxt: true,
-  sitemapSize: 7000
-  // ...other options
-  // https://github.com/iamvishnusankar/next-sitemap#configuration-options
-}
