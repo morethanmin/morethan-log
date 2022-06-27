05049e890183b700d83247a7bde89a75d0297545
frontend-sangmin <lee.sangmin@cashwalk.io>
2022-06-27 12:28:51 +0900

    :mag: static path 제거

--- src/pages/sitemap.xml.js
+++ src/pages/sitemap.xml.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import * as fs from 'fs'
 import { getAllPosts } from '@/lib/notion'
 import { link } from '@/blog.config'
 
@@ -8,39 +7,24 @@ const Sitemap = () => {
 }
 
 export const getServerSideProps = async ({ res }) => {
-
   //post url 가져오기
   const posts = await getAllPosts({ includePages: true })
   const dynamicPaths = posts.map(post => {
     return `${link}/${post.slug}`
   })
 
-  //static url 가져오기
-  const staticPaths = fs
-    .readdirSync("src/pages")
-    .filter((staticPage) => {
-      return ![
-        "api",
-        "page",
-        "tag",
-        "index.js",
-        "[slug].js",
-        "_app.js",
-        "_document.js",
-        "404.js",
-        "sitemap.xml.js",
-      ].includes(staticPage)
-    }).map(el => el.slice(0, -3))
-    .map((staticPagePath) => {
-      return `${link}/${staticPagePath}`
-    })
-
-  const allPaths = [...staticPaths, ...dynamicPaths]
+  const allPaths = [...dynamicPaths]
 
   const sitemap = `
     <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:news="http://www.google.com/schemas/sitemap-news/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml" xmlns:mobile="http://www.google.com/schemas/sitemap-mobile/1.0" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1" xmlns:video="http://www.google.com/schemas/sitemap-video/1.1">
     <url>
       <loc>${link}</loc>
+      <lastmod>${new Date().toISOString()}</lastmod>
+      <changefreq>monthly</changefreq>
+      <priority>1.0</priority>
+    </url>
+    <url>
+      <loc>${link}/feed</loc>
       <lastmod>${new Date().toISOString()}</lastmod>
       <changefreq>monthly</changefreq>
       <priority>1.0</priority>
