8f9e9ee764aac1734979eb29cabfb38691743f9b
front_이상민 <lee.sangmin@cashwalk.io>
2022-12-12 08:40:12 +0900

    feat: update config

--- morethan-log.config.js
+++ morethan-log.config.js
@@ -24,7 +24,7 @@ const CONFIG = {
   // CONFIG configration
   link: 'https://morethan-log.vercel.app',
   since: 2022, // If leave this empty, current year will be used.
-  lang: 'ko-KR', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
+  lang: 'en-US', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
   postsPerPage: 10,
   ogImageGenerateURL: 'https://og-image-korean.vercel.app', // The link to generate OG image, don't end with a slash
   seo: {
