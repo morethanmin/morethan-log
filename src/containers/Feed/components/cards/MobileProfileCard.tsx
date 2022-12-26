398361969b2784ca8eea4806fdc53365e03a83ba
morethanmin <mini4614@gmail.com>
2022-12-26 21:40:48 +0900

    fix: disable profile info

--- src/containers/Feed/components/cards/MobileProfileCard.tsx
+++ src/containers/Feed/components/cards/MobileProfileCard.tsx
@@ -35,7 +35,7 @@ const MobileProfileCard: React.FC<Props> = () => {
             <div className="text-sm">{CONFIG.profile.discription}</div>
           </div>
         </div>
-        <div className="flex">
+        {/* <div className="flex">
           {CONFIG.profile.github && (
             <a
               href={`https://github.com/${CONFIG.profile.github}`}
@@ -76,7 +76,7 @@ const MobileProfileCard: React.FC<Props> = () => {
               <AiFillLinkedin className="text-2xl flex-shrink-0" />
             </a>
           )}
-        </div>
+        </div> */}
       </div>
     </div>
   )
