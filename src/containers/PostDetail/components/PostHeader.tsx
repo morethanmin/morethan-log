81da859cfb7f209424697acc18428701c5fc598f
morethanmin <mini4614@gmail.com>
2023-01-05 22:02:34 +0900

    feat: add profile in post card component

--- src/containers/PostDetail/components/PostHeader.tsx
+++ src/containers/PostDetail/components/PostHeader.tsx
@@ -2,7 +2,7 @@ import CONFIG from '@/morethan-log.config'
 import Tag from '@components/Tag'
 import imageLoader from '@/src/libs/next/imageLoader'
 import { TPost } from '@/src/types'
-import {formatDate} from '@/src/libs/utils'
+import { formatDate } from '@/src/libs/utils'
 import Image from 'next/image'
 import React from 'react'
 
@@ -13,26 +13,40 @@ type Props = {
 const PostHeader: React.FC<Props> = ({ data }) => {
   return (
     <>
+      {/* {data.thumbnail && (
+        <div className="relative w-full pb-[66%] lg:pb-[50%] bg-gray-200 dark:bg-zinc-700 mb-7 rounded-3xl overflow-hidden">
+          <Image
+            src={data.thumbnail}
+            className="object-cover"
+            loader={imageLoader}
+            layout="fill"
+            alt={data.title}
+          />
+        </div>
+      )} */}
       <h1 className="font-bold text-3xl text-black dark:text-white">
         {data.title}
       </h1>
       {data.type[0] !== 'Page' && (
-        <nav className="mt-7 text-gray-500 dark:text-gray-400">
+        <nav className="mt-5 text-gray-500 dark:text-gray-400">
           <div className="flex items-center gap-4 mb-3">
             {data.author && data.author[0] && (
-              <div className="flex items-center gap-2">
-                <Image
-                  className="rounded-full"
-                  src={data.author[0].profile_photo}
-                  alt="profile_photo"
-                  loader={imageLoader}
-                  width={24}
-                  height={24}
-                />
-                <div className="">
-                  {`${data.author[0].last_name}${data.author[0].first_name}`}
+              <>
+                <div className="flex items-center gap-2">
+                  <Image
+                    className="rounded-full"
+                    src={data.author[0].profile_photo}
+                    alt="profile_photo"
+                    loader={imageLoader}
+                    width={24}
+                    height={24}
+                  />
+                  <div className="">
+                    {`${data.author[0].last_name}${data.author[0].first_name}`}
+                  </div>
                 </div>
-              </div>
+                <div className="self-stretch w-px my-1 bg-gray-300"></div>
+              </>
             )}
             <div className=" mr-2 md:ml-0">
               {formatDate(
