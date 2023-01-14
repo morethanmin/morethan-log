f6e29e9ebe5399c9ada77c744e6fc0d5d4e6e789
kvaishak <kvaishak47@gmail.com>
2023-01-14 16:02:07 +0530

    delete 1 file and update 3 files

--- src/containers/CustomError/index.tsx
+++ src/containers/CustomError/index.tsx
@@ -1,25 +1,24 @@
-import Image from 'next/image'
-import React from 'react'
+import Image from "next/image";
+import React from "react";
+import errorPic from "../../../public/images/error.png";
 
 type Props = {
-  errorType?: 'NOT_FOUND' | 'UNKNOWN'
-}
+  errorType?: "NOT_FOUND" | "UNKNOWN";
+};
 
 const CustomError: React.FC<Props> = ({ errorType }) => {
   return (
     <div
-      className={`m-auto max-w-4xl bg-white dark:bg-zinc-700 rounded-3xl py-12 px-6 shadow-md`}
+      className={`m-auto max-w-4xl bg-white dark:bg-zinc-700 rounded-3xl py-12 px-6 mb-8 shadow-md`}
     >
       <div className="py-20 flex flex-col items-center gap-10">
         <div className="text-6xl flex  items-center">
-          <div>4</div>
-          <Image src="/images/error.png" width={60} height={60} alt="error" />
-          <div>4</div>
+          <Image src={errorPic} alt="error" />
         </div>
-        <div className="text-3xl text-gray-500">Post not found</div>
+        <div className="text-3xl text-gray-500">Page Not Found</div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default CustomError
+export default CustomError;
