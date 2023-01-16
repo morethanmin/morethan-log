da248dcffd805da76cdf28393dd542b94eb84135
Vaishak Kaippanchery <25531121+kvaishak@users.noreply.github.com>
2023-01-16 17:45:24 +0530

    [DOCUMENTATION] Documentation Updates (#73)
    * chore: move 2 files, update 2 files and delete 1 file
    
    * docs: update PULL_REQUEST_TEMPLATE.md
    
    * feat: update Footer.tsx

--- src/containers/Feed/components/Footer.tsx
+++ src/containers/Feed/components/Footer.tsx
@@ -14,7 +14,7 @@ const Footer: React.FC<Props> = ({ className }) => {
   return (
     <div className={className}>
       <a
-        href={"https://github.com/morethanmin/morethan-log"}
+        href={`https://github.com/${CONFIG.profile.github}`}
         target="_blank"
         className="text-gray-500 text-sm mt-3"
         rel="noreferrer"
