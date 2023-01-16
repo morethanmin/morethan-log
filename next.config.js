58e094740f2590816ead299033b07e807439dd6b
front_이상민 <lee.sangmin@cashwalk.io>
2023-01-16 18:14:09 +0900

    fix: adding image domain

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   reactStrictMode: false,
   images: {
-    domains: ['www.notion.so', 'lh5.googleusercontent.com'],
+    domains: ['www.notion.so', 'lh5.googleusercontent.com', 's3-us-west-2.amazonaws.com'],
   },
 }
