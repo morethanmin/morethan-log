a008d3392545a3cc1b2f1949d4ccf8c5fb9485de
Jonathan <jchiangconan@gmail.com>
2023-02-12 02:11:32 -0500

    update link again

--- site.config.js
+++ site.config.js
@@ -24,7 +24,7 @@ const CONFIG = {
   },
 
   // CONFIG configration (required)
-  link: 'https://jonathanchiang-cff3yvadp-jchiangconan-gmailcom.vercel.app',
+  link: 'https://jonathanchiang-ca.vercel.app',
   since: 2022, // If leave this empty, current year will be used.
   lang: 'en-US', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
   ogImageGenerateURL: 'https://og-image-korean.vercel.app', // The link to generate OG image, don't end with a slash
