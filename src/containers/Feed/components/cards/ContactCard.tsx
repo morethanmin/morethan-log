52275fcbf3bff37ef9e48d90f8009cf8c898368d
spyhichkas <98157880+spyhichkas@users.noreply.github.com>
2023-02-13 16:23:02 +0330

    Update ContactCard.tsx

--- src/containers/Feed/components/cards/ContactCard.tsx
+++ src/containers/Feed/components/cards/ContactCard.tsx
@@ -70,7 +70,7 @@ const ContactCard: React.FC = () => {
             target="_blank"
             className="p-3 hover:bg-gray-100 dark:hover:bg-zinc-700 rounded-2xl cursor-pointer flex items-center gap-3 text-gray-500 dark:text-white hover:text-black dark:hover:text-white"
           >
-            <FaTelegramPlane className="text-2xl" />
+            <FaTwitter className="text-2xl" />
             <div className="text-sm">twitter</div>
           </a>
         )}
