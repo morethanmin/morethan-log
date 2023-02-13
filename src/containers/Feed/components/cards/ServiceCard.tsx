2fa0b915ce7f9d0959da666b6f4cb30f2e41c59a
spyhichkas <98157880+spyhichkas@users.noreply.github.com>
2023-02-13 14:30:56 +0330

    Update ServiceCard.tsx

--- src/containers/Feed/components/cards/ServiceCard.tsx
+++ src/containers/Feed/components/cards/ServiceCard.tsx
@@ -18,13 +18,13 @@ const ServiceCard: React.FC = () => {
           <div className="text-sm">{CONFIG.projects[0].name}</div>
         </a>
           <a
-          href={`${CONFIG.projects[0].href}`}
+          href={`${CONFIG.projects2[0].href}`}
           rel="noreferrer"
           target="_blank"
           className="p-3 hover:bg-gray-100 dark:hover:bg-zinc-700 rounded-2xl cursor-pointer flex items-center gap-3 text-gray-500 dark:text-white hover:text-black dark:hover:text-white"
         >
           <AiFillCodeSandboxCircle className="text-2xl" />
-          <div className="text-sm">{CONFIG.projects[0].name}</div>
+          <div className="text-sm">{CONFIG.projects2[0].name}</div>
         </a>
       </ul>
     </>
