f4b370ef0112c1766cf9123895daa3bc1cdb6559
spyhichkas <98157880+spyhichkas@users.noreply.github.com>
2023-02-13 09:42:40 +0330

    Update index.tsx

--- src/containers/Feed/components/cards/index.tsx
+++ src/containers/Feed/components/cards/index.tsx
@@ -1,6 +1,7 @@
 import ProfileCard from './ProfileCard'
+import ProfileCard from './newprof'
 import ContactCard from './ContactCard'
 import ServiceCard from './ServiceCard'
 import MobileProfileCard from './MobileProfileCard'
 
-export { ProfileCard, ContactCard, ServiceCard, MobileProfileCard }
+export { ProfileCard, newprof, ContactCard, ServiceCard, MobileProfileCard }
