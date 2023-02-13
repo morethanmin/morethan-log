b6fb2c6981a9ce029dab7dcaf93fc59288ffcb16
spyhichkas <98157880+spyhichkas@users.noreply.github.com>
2023-02-13 15:04:22 +0330

    Update ContactCard.tsx

--- src/containers/Feed/components/cards/ContactCard.tsx
+++ src/containers/Feed/components/cards/ContactCard.tsx
@@ -5,7 +5,7 @@ import {
   AiOutlineGithub,
   AiOutlineMail,
   AiFillLinkedin,
-  AiFillSkype,
+  AiOutlineSkype,
 } from "react-icons/ai"
 
 const ContactCard: React.FC = () => {
@@ -31,7 +31,7 @@ const ContactCard: React.FC = () => {
             target="_blank"
             className="p-3 hover:bg-gray-100 dark:hover:bg-zinc-700 rounded-2xl cursor-pointer flex items-center gap-3 text-gray-500 dark:text-white hover:text-black dark:hover:text-white "
           >
-            <AiFillSkype className="text-2xl" />
+            <AiOutlineSkype className="text-2xl" />
             <div className="text-sm">skype</div>
           </a>
         )}
