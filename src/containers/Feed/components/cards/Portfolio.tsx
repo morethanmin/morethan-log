ddc791a1e68fae3f16adc90420e22c79b8426ca6
spyhichkas <98157880+spyhichkas@users.noreply.github.com>
2023-02-13 11:19:59 +0330

    Update Portfolio.tsx

--- src/containers/Feed/components/cards/Portfolio.tsx
+++ src/containers/Feed/components/cards/Portfolio.tsx
@@ -12,14 +12,14 @@ const Portfolio: React.FC<Props> = ({ className }) => {
       <div className="p-1 mb-3 dark:text-white">📈 Portfolio</div>
       <div className="w-full md:p-4 lg:p-4 rounded-2xl bg-white dark:bg-zinc-700 mb-9">
         <div className="relative w-full after:content-[''] after:block after:pb-[100%]">
-          <Image src={CONFIG.profile.image1} layout="fill" alt="" />
+          <Image src={CONFIG.portfolio.image} layout="fill" alt="" />
         </div>
         <div className="bg-white p-2 flex flex-col items-center dark:bg-zinc-700 dark:text-white">
-          <div className=" text-xl italic font-bold">{CONFIG.profile.name1}</div>
+          <div className=" text-xl italic font-bold">{CONFIG.portfolio.name}</div>
           <div className="text-sm mb-4 text-gray-500 dark:text-gray-400">
-            {CONFIG.profile.role1}
+            {CONFIG.portfolio.description}
           </div>
-          <div className="text-sm mb-2">{CONFIG.profile.bio1}</div>
+          <div className="text-sm mb-2">{CONFIG.profile.caption}</div>
         </div>
       </div>
     </div>
