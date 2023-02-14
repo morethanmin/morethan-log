75dbeef85bdb041ff4be53162bb7a5768e7d30d0
Zakops-com <98157880+Zakops-com@users.noreply.github.com>
2023-02-14 10:27:22 +0330

    Update index.tsx

--- src/containers/Feed/components/cards/index.tsx
+++ src/containers/Feed/components/cards/index.tsx
@@ -1,5 +1,5 @@
 import ProfileCard from './ProfileCard'
-import Portfolio from './Certificates'
+import Certificates from './Certificates'
 import ContactCard from './ContactCard'
 import ServiceCard from './ServiceCard'
 import MobileProfileCard from './MobileProfileCard'
