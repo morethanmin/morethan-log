8e06ffadf96afc2de66d095367c12ec6b1243f1b
Zakops-com <98157880+Zakops-com@users.noreply.github.com>
2023-02-14 11:19:11 +0330

    Update Certificates.tsx

--- src/containers/Feed/components/cards/Certificates.tsx
+++ src/containers/Feed/components/cards/Certificates.tsx
@@ -10,9 +10,9 @@ const Certificates: React.FC<Props> = ({ className }) => {
   return (
     <div className={className}>
       <div className="p-1 mb-3 dark:text-white">🖺 Certificates</div>
-      <div className="w-full md:p-4 lg:p-4 rounded-2xl ">
+      <div className="w-full md:p-4 lg:p-4 rounded-2xl bg-white dark:bg-zinc-700 mb-9">
         <div className="relative w-full after:content-[''] after:block after:pb-[100%]">
-          <Image src={CONFIG.Certificates.image} layout="fill" alt="" />
+          <Image src={CONFIG.Certificates.image} layout="fill" alt="Certificates" />
         </div>
         <div className="bg-white p-2 flex flex-col items-center dark:bg-zinc-700 dark:text-white">
           <div className=" text-xl italic font-bold">{CONFIG.Certificates.name}</div>
