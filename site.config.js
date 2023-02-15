65cb5ecab47f93230f75b8108c1cc0bce5c93c35
Zakops-com <98157880+Zakops-com@users.noreply.github.com>
2023-02-15 10:03:06 +0330

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,7 +1,7 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: 'Mike',
+    name: 'Zak',
     image: '/avatar.svg',  // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
     role: 'Devops',
     bio: 'I Am DevOps Engineer',
