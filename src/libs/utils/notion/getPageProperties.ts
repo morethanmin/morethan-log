5f6304c40c7a37c3f33991e81941fe3b4e952f79
Octoping <mooner925@gmail.com>
2023-02-26 21:57:57 +0900

    refactor: getPageProperties

--- src/libs/utils/notion/getPageProperties.ts
+++ src/libs/utils/notion/getPageProperties.ts
@@ -8,78 +8,115 @@ async function getPageProperties(
   block: BlockMap,
   schema: CollectionPropertySchemaMap
 ) {
-  const api = new NotionAPI()
   const rawProperties = Object.entries(block?.[id]?.value?.properties || [])
-  const excludeProperties = ["date", "select", "multi_select", "person", "file"]
-  const properties: any = {}
-  for (let i = 0; i < rawProperties.length; i++) {
-    const [key, val]: any = rawProperties[i]
-    properties.id = id
-    if (schema[key]?.type && !excludeProperties.includes(schema[key].type)) {
-      properties[schema[key].name] = getTextContent(val)
-    } else {
-      switch (schema[key]?.type) {
-        case "file": {
-          try {
-            const Block = block?.[id].value
-            const url: string = val[0][1][0][1]
-            const newurl = customMapImageUrl(url, Block)
-            properties[schema[key].name] = newurl
-          } catch (error) {
-            properties[schema[key].name] = undefined
-          }
-          break
-        }
-        case "date": {
-          const dateProperty: any = getDateValue(val)
-          delete dateProperty.type
-          properties[schema[key].name] = dateProperty
-          break
-        }
-        case "select": {
-          const selects = getTextContent(val)
-          if (selects[0]?.length) {
-            properties[schema[key].name] = selects.split(",")
-          }
-          break
-        }
-        case "multi_select": {
-          const selects = getTextContent(val)
-          if (selects[0]?.length) {
-            properties[schema[key].name] = selects.split(",")
-          }
-          break
-        }
-        case "person": {
-          const rawUsers = val.flat()
 
-          const users = []
-          for (let i = 0; i < rawUsers.length; i++) {
-            if (rawUsers[i][0][1]) {
-              const userId = rawUsers[i][0]
-              const res: any = await api.getUsers(userId)
-              const resValue =
-                res?.recordMapWithRoles?.notion_user?.[userId[1]]?.value
-              const user = {
-                id: resValue?.id,
-                name:
-                  resValue?.name ||
-                  `${resValue?.family_name}${resValue?.given_name}` ||
-                  undefined,
-                profile_photo: resValue?.profile_photo || null,
-              }
-              users.push(user)
-            }
-          }
-          properties[schema[key].name] = users
-          break
+  const parsedProperties = await Promise.all(
+    rawProperties.map(([key, val]) =>
+      PropertyHandler.handle(schema[key]?.type, val, id, block)
+    )
+  )
+
+  const propertyKeys = rawProperties.map((property) => schema[property[0]].name)
+
+  return parsedProperties
+    .filter((property) => property !== null)
+    .reduce(
+      (acc, property, idx) => {
+        return {
+          ...acc,
+          [propertyKeys[idx]]: property,
         }
-        default:
-          break
-      }
+      },
+      { id }
+    )
+}
+
+abstract class PropertyHandler {
+  static handle(type: string, val: any, id: string, block: BlockMap) {
+    switch (type) {
+      case "file":
+        return filePropertyHandler.parse(val, id, block)
+      case "date":
+        return datePropertyHandler.parse(val)
+      case "select":
+      case "multi_select":
+        return selectPropertyHandler.parse(val)
+      case "person":
+        return personPropertyHandler.parse(val)
+      default:
+        return getTextContent(val)
     }
   }
-  return properties
+
+  abstract parse(...args: any): any
 }
+
+class FilePropertyHandler extends PropertyHandler {
+  parse(val: any, id: string, block: BlockMap) {
+    try {
+      const Block = block?.[id].value
+      const url: string = val[0][1][0][1]
+      const newurl = customMapImageUrl(url, Block)
+      return newurl
+    } catch (error) {
+      return null
+    }
+  }
+}
+
+class DatePropertyHandler extends PropertyHandler {
+  parse(val: any[]) {
+    const dateProperty: any = getDateValue(val)
+    delete dateProperty.type
+    return dateProperty
+  }
+}
+
+class SelectPropertyHandler extends PropertyHandler {
+  parse(val: any) {
+    const selects = getTextContent(val)
+    if (selects[0]?.length) {
+      return selects.split(",")
+    }
+    return null
+  }
+}
+
+class PersonPropertyHandler extends PropertyHandler {
+  api = new NotionAPI()
+
+  async parse(val: any) {
+    const rawUsers: any[] = val.flat()
+    const users = []
+
+    for (const element of rawUsers) {
+      if (element[0][1]) {
+        const userId = element[0]
+        const res: any = await this.api.getUsers(userId)
+
+        const resValue =
+          res?.recordMapWithRoles?.notion_user?.[userId[1]]?.value
+
+        const user = {
+          id: resValue?.id,
+          name:
+            resValue?.name ||
+            `${resValue?.family_name}${resValue?.given_name}` ||
+            undefined,
+          profile_photo: resValue?.profile_photo || null,
+        }
+
+        users.push(user)
+      }
+    }
+
+    return users
+  }
+}
+
+const filePropertyHandler = new FilePropertyHandler()
+const datePropertyHandler = new DatePropertyHandler()
+const selectPropertyHandler = new SelectPropertyHandler()
+const personPropertyHandler = new PersonPropertyHandler()
 
 export { getPageProperties as default }
