956d2b4297226896a9f12b29458be214037e3cf5
KKUMI <gayoung801@naver.com>
2023-03-01 17:11:09 +0900

    config setting

--- site.config.js
+++ site.config.js
@@ -1,25 +1,25 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: "morethanmin",
+    name: "Gayoung Kim",
     image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
-    role: "frontend developer",
-    bio: "I develop everything using node.",
-    email: "morethanmin.dev@gmail.com",
-    linkedin: "morethanmin",
-    github: "morethanmin",
-    instagram: "",
+    role: "Backend developer",
+    bio: "I develop everything using java.",
+    email: "gayoung801@naver.com",
+    linkedin: "gayoung",
+    github: "JUGGUM",
+    instagram: "gaaaa_o",
   },
   projects: [
     {
-      name: `morethan-log`,
+      name: `ggumi-log`,
       href: "https://github.com/morethanmin/morethan-log",
     },
   ],
   // blog setting (required)
   blog: {
-    title: "morethan-log",
-    description: "welcome to morethan-log!",
+    title: "ggumi-log",
+    description: "welcome to ggumi-log!",
     theme: "light", // ['light', 'dark', 'auto']
   },
 
