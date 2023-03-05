dc37c241f58bda08c868953574ffafdf63d7770f
sd990307@gmail.com <songdoyoon@songdoyun-ui-MacBook-Pro.local>
2023-03-05 17:13:03 +0900

    Initial Configuration_9

--- src/components/Layout/Header/Logo.tsx
+++ src/components/Layout/Header/Logo.tsx
@@ -11,7 +11,7 @@ const Logo: React.FC<Props> = () => {
         <div className="flex items-center">
           <div className="ml-2 text-black dark:text-white header-name">
             {CONFIG.blog.title}
-            <Image href="https://www.google.com/url?sa=i&url=http%3A%2F%2Fwww.motorpress.co.kr%2Fnews%2FarticleView.html%3Fidxno%3D1839&psig=AOvVaw1cqsDZwdwXogjo0UGQ9QeW&ust=1678090268538000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCJCb6MSrxP0CFQAAAAAdAAAAABAE"></Image>
+            <img src="https://www.google.com/url?sa=i&url=http%3A%2F%2Fwww.motorpress.co.kr%2Fnews%2FarticleView.html%3Fidxno%3D1839&psig=AOvVaw1cqsDZwdwXogjo0UGQ9QeW&ust=1678090268538000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCJCb6MSrxP0CFQAAAAAdAAAAABAE"></img>
           </div>
         </div>
       </a>
