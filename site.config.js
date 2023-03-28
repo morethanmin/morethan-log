89dfcad34c7fef452b5922b915255f0b435a2d03
ggiho <48872962+ggiho@users.noreply.github.com>
2023-03-28 20:50:16 +0900

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -18,14 +18,14 @@ const CONFIG = {
   ],
   // blog setting (required)
   blog: {
-    title: "giho-blog",
-    description: "welcome to giho-blog!",
-    theme: "light", // ['light', 'dark', 'auto']
+    title: "BLOG",
+    description: "welcome to giho blog!",
+    theme: "dark", // ['light', 'dark', 'auto']
   },
 
   // CONFIG configration (required)
   link: "https://morethan-log.vercel.app",
-  since: 2022, // If leave this empty, current year will be used.
+  since: 2023, // If leave this empty, current year will be used.
   lang: "en-US", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
   ogImageGenerateURL: "https://og-image-korean.vercel.app", // The link to generate OG image, don't end with a slash
   seo: {
