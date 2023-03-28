f230378bb8584dc69f08fd2533560404a59cb642
ggiho <48872962+ggiho@users.noreply.github.com>
2023-03-28 10:30:00 +0900

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,13 +1,13 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: "morethanmin",
+    name: "giho",
     image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
-    role: "frontend developer",
+    role: "DBA",
     bio: "I develop everything using node.",
-    email: "morethanmin.dev@gmail.com",
-    linkedin: "morethanmin",
-    github: "morethanmin",
+    email: "wasd92@naver.com",
+    linkedin: "",
+    github: "",
     instagram: "",
   },
   projects: [
