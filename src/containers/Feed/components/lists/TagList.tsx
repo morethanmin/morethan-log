62a4b2f65a986840f69a5c2bcf352314ae6c36d3
ggiho <48872962+ggiho@users.noreply.github.com>
2023-03-28 21:22:28 +0900

    Update TagList.tsx

--- src/containers/Feed/components/lists/TagList.tsx
+++ src/containers/Feed/components/lists/TagList.tsx
@@ -36,19 +36,24 @@ const TagList: React.FC<Props> = ({ className, data }) => {
     <div className={className}>
       <div className="hidden lg:block p-1 mb-3 dark:text-white">🏷️ Tags</div>
       <ul className="cursor-pointer gap-1 flex mobile-x-scroll lg:block mb-6">
-        { for (const [key, value] of Object.entries(data)) {
-            <li
-            key={key}
-            className={`text-sm p-1 px-4 my-1 flex-shrink-0 rounded-xl text-gray-500 dark:text-white hover:bg-gray-200 dark:hover:bg-zinc-800 ${
-              key === currentTag &&
-              "text-black bg-white dark:bg-zinc-700 hover:bg-white dark:hover:bg-zinc-700"
-            }`}
-            onClick={() => handleClickTag(key)}
-          >
-            <a>{key} ({value})</a>
-          </li>
-          }
-        }
+//         {Object.keys(data).map((key, val) => (
+//           <li
+//             key={key}
+//             className={`text-sm p-1 px-4 my-1 flex-shrink-0 rounded-xl text-gray-500 dark:text-white hover:bg-gray-200 dark:hover:bg-zinc-800 ${
+//               key === currentTag &&
+//               "text-black bg-white dark:bg-zinc-700 hover:bg-white dark:hover:bg-zinc-700"
+//             }`}
+//             onClick={() => handleClickTag(key)}
+//           >
+//             <a>{key}({val})</a>
+//             <a>{key} ({val})</a>
+//           </li>
+//         ))}
+          {
+            for (const [key, value] of Object.entries(data)) {
+              console.log(`${key}: ${value}`);
+            }
+           }
       </ul>
     </div>
   )
