24c032065b326b5478ae7bbfbdf10c09e35f3f9f
linnil <39083566+linnilsupak@users.noreply.github.com>
2023-03-29 10:29:36 +0700

    rename all env use in next build to NEXT_PUBLIC_

--- site.config.js
+++ site.config.js
@@ -41,13 +41,13 @@ const CONFIG = {
   googleAnalytics: {
     enable: false,
     config: {
-      measurementId: process.env.GOOGLE_MEASUREMENT_ID || "",
+      measurementId: process.env.NEXT_PUBLIC_GOOGLE_MEASUREMENT_ID || "",
     },
   },
   googleSearchConsole: {
     enable: false,
     config: {
-      siteVerification: process.env.GOOGLE_SITE_VERIFICATION || "",
+      siteVerification: process.env.NEXT_PUBLIC_GOOGLE_SITE_VERIFICATION || "",
     },
   },
   utterances: {
