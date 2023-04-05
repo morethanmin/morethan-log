de3256c36090b22f4a7513842ec0da896b6b8296
HyomYom <hyomyang@gmail.com>
2023-04-06 03:51:59 +0900

    update

--- site.config.js
+++ site.config.js
@@ -1,13 +1,13 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: "morethanmin",
+    name: "pagooo",
     image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
-    role: "frontend developer",
-    bio: "I develop everything using node.",
-    email: "morethanmin.dev@gmail.com",
-    linkedin: "morethanmin",
-    github: "morethanmin",
+    role: "Backend developer",
+    bio: "I develop everything using Spring.",
+    email: "hyomyang@gmail.com",
+    linkedin: "pagoooo",
+    github: "HyomYom",
     instagram: "",
   },
   projects: [
@@ -18,8 +18,8 @@ const CONFIG = {
   ],
   // blog setting (required)
   blog: {
-    title: "morethan-log",
-    description: "welcome to morethan-log!",
+    title: "paooo-log",
+    description: "welcome to pagooo-log!",
     theme: "light", // ['light', 'dark', 'auto']
   },
 
