d692cf33bc82113c0501dd4aad8ef6713a1f3f51
ggiho <48872962+ggiho@users.noreply.github.com>
2023-04-10 21:25:22 +0900

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -13,7 +13,7 @@ const CONFIG = {
   projects: [
     {
       name: `morethan-log`,
-      href: "https://github.com/morethanmin/morethan-log",
+      href: "https://github.com/ggiho/morethan-log",
     },
   ],
   // blog setting (required)
