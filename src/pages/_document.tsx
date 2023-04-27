22fb3440c94368dee55a79169d21ed007d7740af
GyeongmiLee <dl_rudal@naver.com>
2023-04-27 20:22:37 +0900

    test

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -12,11 +12,11 @@ class MyDocument extends Document {
             href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard/dist/web/static/pretendard-dynamic-subset.css"
           />
           <link rel="icon" href="" /> */}
-          <link
+          {/* <link
             rel="apple-touch-icon"
             sizes="192x192"
             href="/apple-touch-icon.png"
-          ></link>
+          ></link> */}
           <link
             rel="alternate"
             type="application/rss+xml"
