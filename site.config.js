a5503efc5cb778bb5713b20f87cd36b6b6281246
GyeongmiLee <dl_rudal@naver.com>
2023-04-27 11:41:09 +0900

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,7 +1,7 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: "morethanmin",
+    name: "GyeongmiLee",
     image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
     role: "frontend developer",
     bio: "I develop everything using node.",
