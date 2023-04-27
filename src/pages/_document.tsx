e0ee6fcfe52b1a23ee6f6e5f580b651d1bbcbff7
GyeongmiLee <dl_rudal@naver.com>
2023-04-27 12:28:38 +0900

    Update _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -11,7 +11,7 @@ class MyDocument extends Document {
             as="font"
             href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard/dist/web/static/pretendard-dynamic-subset.css"
           />
-          <link rel="icon" href="/favicon.ico" />
+          {/* <link rel="icon" href="/favicon.ico" /> */}
           <link
             rel="apple-touch-icon"
             sizes="192x192"
