a6e156e50c9c812f2fa8b9437fb92503404310c6
davie <davie@channel.io>
2023-05-29 22:51:17 +0900

    fix: error for projects contents

--- site.config.js
+++ site.config.js
@@ -11,10 +11,10 @@ const CONFIG = {
     instagram: "",
   },
   projects: [
-    // {
-    //   name: `morethan-log`,
-    //   href: "https://github.com/morethanmin/morethan-log",
-    // },
+    {
+      name: `morethan-log`,
+      href: "https://github.com/morethanmin/morethan-log",
+    },
   ],
   // blog setting (required)
   blog: {
