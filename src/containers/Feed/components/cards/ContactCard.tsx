9be274113488a339ea66d9fee33e634a0259abed
wlals1126 <103996321+wlals1126@users.noreply.github.com>
2023-06-29 12:23:07 +0900

    :poop: :: ContactCard 수정

--- src/containers/Feed/components/cards/ContactCard.tsx
+++ src/containers/Feed/components/cards/ContactCard.tsx
@@ -3,8 +3,7 @@ import React from "react"
 import {
   AiOutlineInstagram,
   AiOutlineGithub,
-  AiOutlineMail,
-  AiFillLinkedin,
+  AiOutlineMail
 } from "react-icons/ai"
 
 const ContactCard: React.FC = () => {
@@ -43,17 +42,6 @@ const ContactCard: React.FC = () => {
           >
             <AiOutlineMail className="text-2xl flex-shrink-0" />
             <div className="text-sm">email</div>
-          </a>
-        )}
-        {CONFIG.profile.linkedin && (
-          <a
-            href={`https://www.linkedin.com/in/${CONFIG.profile.linkedin}`}
-            rel="noreferrer"
-            target="_blank"
-            className="  overflow-hidden p-3 hover:bg-gray-100 dark:hover:bg-zinc-700 rounded-2xl cursor-pointer flex items-center gap-3 text-gray-500 dark:text-white hover:text-black dark:hover:text-white"
-          >
-            <AiFillLinkedin className="text-2xl flex-shrink-0" />
-            <div className="text-sm">linkedin</div>
           </a>
         )}
       </ul>
