df3e828b3f23836d754d21831c3219797c464a8f
Poori nuna <38172712+poori-nuna@users.noreply.github.com>
2023-06-30 15:38:37 +0900

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -24,7 +24,7 @@ const CONFIG = {
   },
 
   // CONFIG configration (required)
-  link: "https://kyum-blog.vercel.app",
+  link: "https://poori-nuna-blog.vercel.app",
   since: 2023, // If leave this empty, current year will be used.
   lang: "en-US", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
   ogImageGenerateURL: "https://og-image-korean.vercel.app", // The link to generate OG image, don't end with a slash
