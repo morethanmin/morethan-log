40fd2d44a9d90f3bcb2b3920f39ef29a5b828dde
Poori nuna <38172712+poori-nuna@users.noreply.github.com>
2023-07-01 14:28:08 +0900

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -45,7 +45,7 @@ const CONFIG = {
     },
   },
   googleSearchConsole: {
-    enable: false,
+    enable: true,
     config: {
       siteVerification: process.env.NEXT_PUBLIC_GOOGLE_SITE_VERIFICATION || "",
     },
