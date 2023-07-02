91ab5d2ae3cdb443b42b97d638af46c9ab3f5a3a
Bong <qhdgkdbs@gmail.com>
2023-07-02 18:29:47 +0900

    fix: remove a tag in link tag
    - for next13

--- src/components/Layout/Header/Logo.tsx
+++ src/components/Layout/Header/Logo.tsx
@@ -6,14 +6,12 @@ type Props = {}
 
 const Logo: React.FC<Props> = () => {
   return (
-    <Link href="/">
-      <a aria-label={CONFIG.blog.title}>
-        <div className="flex items-center">
-          <div className="ml-2 text-black dark:text-white header-name">
-            {CONFIG.blog.title}
-          </div>
+    <Link href="/" aria-label={CONFIG.blog.title}>
+      <div className="flex items-center">
+        <div className="ml-2 text-black dark:text-white header-name">
+          {CONFIG.blog.title}
         </div>
-      </a>
+      </div>
     </Link>
   )
 }
