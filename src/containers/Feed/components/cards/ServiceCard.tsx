7fa718a641b0d0739fc167e9c322710f2ce5e09b
Derrick Song <96987446+SongDerrick@users.noreply.github.com>
2023-07-05 14:47:30 +0900

    service fixed

--- src/containers/Feed/components/cards/ServiceCard.tsx
+++ src/containers/Feed/components/cards/ServiceCard.tsx
@@ -17,6 +17,24 @@ const ServiceCard: React.FC = () => {
           <AiFillCodeSandboxCircle className="text-2xl" />
           <div className="text-sm">{CONFIG.projects[0].name}</div>
         </a>
+        <a
+          href={`${CONFIG.projects[1].href}`}
+          rel="noreferrer"
+          target="_blank"
+          className="p-3 hover:bg-gray-100 dark:hover:bg-zinc-700 rounded-2xl cursor-pointer flex items-center gap-3 text-gray-500 dark:text-white hover:text-black dark:hover:text-white"
+        >
+          <AiFillCodeSandboxCircle className="text-2xl" />
+          <div className="text-sm">{CONFIG.projects[1].name}</div>
+        </a>
+        <a
+          href={`${CONFIG.projects[2].href}`}
+          rel="noreferrer"
+          target="_blank"
+          className="p-3 hover:bg-gray-100 dark:hover:bg-zinc-700 rounded-2xl cursor-pointer flex items-center gap-3 text-gray-500 dark:text-white hover:text-black dark:hover:text-white"
+        >
+          <AiFillCodeSandboxCircle className="text-2xl" />
+          <div className="text-sm">{CONFIG.projects[2].name}</div>
+        </a>
       </ul>
     </>
   )
