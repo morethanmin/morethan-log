a9963aed70c0d08d6e1fd86c5c37f89cf8332765
JunYoung <68208417+JunYou-Park@users.noreply.github.com>
2023-07-06 16:41:53 +0900

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -2,7 +2,7 @@ const CONFIG = {
   // profile setting (required)
   profile: {
     name: "JunYoung",
-    image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
+    image: "/avatar2.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
     role: "Android Developer",
     bio: "All of my thing is Android.",
     email: "jypjun12@gmail.com",
