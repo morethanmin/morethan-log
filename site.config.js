5ebc78865ee917a8fac122f2bf304288293e1827
YUJIN JI <131226548+zidoopal@users.noreply.github.com>
2023-07-23 20:45:06 +0900

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -12,8 +12,8 @@ const CONFIG = {
   },
   projects: [
     {
-      name: `morethan-log`,
-      href: "https://github.com/morethanmin/morethan-log",
+      name: `Doopal :)`,
+      href: "",
     },
   ],
   // blog setting (required)
