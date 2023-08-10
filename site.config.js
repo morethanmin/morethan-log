40878bb6c1d587aaf9cd75a282a2b62ae8f3f631
IrvanMaulana99 <lenovo.irvan99@gmail.com>
2023-08-10 11:50:43 +0700

    ubah informasi

--- site.config.js
+++ site.config.js
@@ -1,32 +1,33 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: "morethanmin",
-    image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
-    role: "frontend developer",
+    name: "Irvan Maulana",
+    image: "/avatar.png", 
+    role: "UI/UX Designer",
     bio: "I develop everything using node.",
-    email: "morethanmin.dev@gmail.com",
-    linkedin: "morethanmin",
-    github: "morethanmin",
-    instagram: "",
+    email: "irvanmaulana.studio@protonmail.com",
+    linkedin: "irvan-maulana4869",
+    github: "IrvanMaulana99",
+    instagram: "irvanmaulana.studio",
   },
   projects: [
     {
-      name: `morethan-log`,
-      href: "https://github.com/morethanmin/morethan-log",
+      name: `notion-2`,
+      href: "https://github.com/IrvanMaulana99/notion-2",
     },
   ],
   // blog setting (required)
   blog: {
-    title: "morethan-log",
-    description: "welcome to morethan-log!",
+    title: "Irvan Maulana Personal Blog",
+    description: "welcome to my personal blog",
+    theme: "dark",
   },
 
   // CONFIG configration (required)
   link: "https://morethan-log.vercel.app",
-  since: 2022, // If leave this empty, current year will be used.
+  since: 2023, // If leave this empty, current year will be used.
   lang: "en-US", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
-  ogImageGenerateURL: "https://og-image-korean.vercel.app", // The link to generate OG image, don't end with a slash
+  ogImageGenerateURL: "https://og-image-korean.vercel.app/Irvan%20Maulana%20Personal%20Blog.png?theme=light&md=1&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg&images=https%3A%2F%2Fitbabble.files.wordpress.com%2F2021%2F11%2Fnotion.png", // The link to generate OG image, don't end with a slash
 
   // notion configuration (required)
   notionConfig: {
@@ -49,7 +50,7 @@ const CONFIG = {
   utterances: {
     enable: true,
     config: {
-      repo: "morethanmin/morethan-log",
+      repo: "IrvanMaulana99/notion-2",
       "issue-term": "og:title",
       label: "💬 Utterances",
     },
