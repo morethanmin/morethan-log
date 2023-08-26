0543cc404fef302d3c43f3dc0e8a5f5d035aa150
UkkingDataLog <143287599+UkkingDataLog@users.noreply.github.com>
2023-08-27 04:40:19 +0900

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,24 +1,24 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: "morethanmin",
+    name: "UkkingData",
     image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
-    role: "frontend developer",
-    bio: "I develop everything using node.",
-    email: "morethanmin.dev@gmail.com",
-    linkedin: "morethanmin",
-    github: "morethanmin",
+    role: "data scientist",
+    bio: "I use data to predict the future.\n- Statistics for data analysis\n- Python/R",
+    email: "ukkingdatalog@gmail.com",
+    linkedin: "-",
+    github: "ukkingdatalog",
     instagram: "",
   },
   projects: [
     {
-      name: `morethan-log`,
-      href: "https://github.com/morethanmin/morethan-log",
+      name: `ukking-data-log`,
+      href: "https://github.com/UkkingDataLog/morethan-log",
     },
   ],
   // blog setting (required)
   blog: {
-    title: "morethan-log",
+    title: "ukking-data-logg",
     description: "welcome to morethan-log!",
   },
 
