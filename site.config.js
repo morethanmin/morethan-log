5e77315d2dac2190b6276c9ff55e8bd97e898ec9
pajiyeee <jijiji941@gmail.com>
2023-08-29 11:22:44 +0900

    config update

--- site.config.js
+++ site.config.js
@@ -57,7 +57,7 @@ const CONFIG = {
   utterances: {
     enable: false,
     config: {
-      repo: process.env.NEXT_PUBLIC_UTTERANCES_REPO,
+      repo: process.env.NEXT_PUBLIC_UTTERANCES_REPO || "",
       "issue-term": "og:title",
       label: "💬 Utterances",
     },
