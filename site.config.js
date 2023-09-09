fe3f3d302cf0dbf3a9343ca0137f5b48060f4f58
untitled-001 <untitled.jsp@gmail.com>
2023-09-09 18:29:23 +0900

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -26,7 +26,7 @@ const CONFIG = {
   link: "https://morethan-log.vercel.app",
   since: 2022, // If leave this empty, current year will be used.
   lang: "en-US", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
-  ogImageGenerateURL: "https://og-image-korean.vercel.app", // The link to generate OG image, don't end with a slash
+  ogImageGenerateURL: "https://og-image-korean.vercel.app/hello.png", // The link to generate OG image, don't end with a slash
 
   // notion configuration (required)
   notionConfig: {
