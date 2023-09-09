ae0a0a6178fb53473b2609d8d39e6f58dea36a1b
untitled-001 <untitled.jsp@gmail.com>
2023-09-09 18:39:15 +0900

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -23,7 +23,7 @@ const CONFIG = {
   },
 
   // CONFIG configration (required)
-  link: "https://morethan-log.vercel.app",
+  link: "https://tasdfao.vercel.app",
   since: 2022, // If leave this empty, current year will be used.
   lang: "en-US", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
   ogImageGenerateURL: "https://og-image-korean.vercel.app/hello.png", // The link to generate OG image, don't end with a slash
