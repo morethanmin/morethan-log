084eab6b3091914c9109eee36fcc98ed4058f3c9
SuppleBase <147074552+SuppleBase@users.noreply.github.com>
2023-10-18 14:20:09 +0200

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,30 +1,30 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: "morethanmin",
+    name: "SuppleBase",
     image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
-    role: "frontend developer",
+    role: "Engineering Manager | Cloud & Data Analytics",
     bio: "I develop everything using node.",
-    email: "morethanmin.dev@gmail.com",
-    linkedin: "morethanmin",
-    github: "morethanmin",
-    instagram: "",
+    email: "supplebase@outlook.com",
+    linkedin: "linkedinprofile",
+    github: "gitprofile",
+    instagram: "supplebase",
   },
   projects: [
     {
-      name: `morethan-log`,
+      name: `ProjectGIT`,
       href: "https://github.com/morethanmin/morethan-log",
     },
   ],
   // blog setting (required)
   blog: {
-    title: "morethan-log",
-    description: "welcome to morethan-log!",
+    title: "SuppleBase",
+    description: "Welcome to SuppleBase, Tech blog",
   },
 
   // CONFIG configration (required)
   link: "https://morethan-log.vercel.app",
-  since: 2022, // If leave this empty, current year will be used.
+  since: 2023, // If leave this empty, current year will be used.
   lang: "en-US", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
   ogImageGenerateURL: "https://og-image-korean.vercel.app", // The link to generate OG image, don't end with a slash
 
