50aca6751efeb07fdba09d973689df84775c8960
nine_floor <less0805@gmail.com>
2023-10-31 03:34:43 +0900

    fix

--- site.config.js
+++ site.config.js
@@ -11,8 +11,8 @@ const CONFIG = {
     instagram: "nine_floor",
   },
   projects: [{
-    name: 'github',
-    href: 'https://github.com/ninefloor',
+    name: '',
+    href: '',
   }
   ],
   // blog setting (required)
