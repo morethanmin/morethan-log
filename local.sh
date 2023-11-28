1c507b0e865032adfb525db9d8de00917e4994e7
chrisae9 <chrisae9@gmail.com>
2023-11-28 01:32:46 +0000

    docker updates

--- local.sh
+++ local.sh
@@ -1,4 +1,6 @@
-docker run -it --name chis-blog-dev --rm -v $(pwd):/app -w /app -p 2565:3000 --env-file .env node:18-alpine /bin/sh
+# spins up a local dev shell
 
-#yarn install
-#yarn dev
+# yarn install
+# yarn dev
+
+docker run -it --name chis-blog-dev --rm -v $(pwd):/app -w /app -p 2565:3000 --env-file .env node:18-alpine /bin/sh
