8c5cdc39751a5234d2cd4ce8848f75413d9ba8f7
Natale Mastrogiacomo <127247019+GameRule17@users.noreply.github.com>
2023-11-30 22:37:56 +0100

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -2,7 +2,7 @@ const CONFIG = {
   // profile setting (required)
   profile: {
     name: "Natale Mastrogiacomo",
-    image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
+    image: "/logo.png", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
     role: "IT Student",
     bio: "",
     email: "natanmast@gmail.com",
