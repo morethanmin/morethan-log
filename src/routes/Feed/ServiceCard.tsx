4f506c5751b27862e6dfbd02ea8ad3367b4f7f79
Natale Mastrogiacomo <127247019+GameRule17@users.noreply.github.com>
2023-12-01 10:28:09 +0100

    Update ServiceCard.tsx

--- src/routes/Feed/ServiceCard.tsx
+++ src/routes/Feed/ServiceCard.tsx
@@ -9,7 +9,7 @@ const ServiceCard: React.FC = () => {
   return (
     <>
       <StyledTitle>
-        <Emoji>🌟</Emoji> Service
+        <Emoji>🌟</Emoji> Projects
       </StyledTitle>
       <StyledWrapper>
         {CONFIG.projects.map((project, idx) => (
