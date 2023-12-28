47750ad8fd32f08083a54702b2090d8aff78ccf0
Vignesh Nandakumar <nviki001@gmail.com>
2023-12-28 21:52:54 +0530

    Adding privacy policy page for google adsense & changing logo

--- site.config.js
+++ site.config.js
@@ -2,7 +2,7 @@ const CONFIG = {
   // profile setting (required)
   profile: {
     name: `${process.env.NEXT_PUBLIC_NICKNAME}`,
-    image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
+    image: "/logo.png", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
     role: `${process.env.NEXT_PUBLIC_PROFILE_ROLE}`,
     bio: `${process.env.NEXT_PUBLIC_BLOG_BIO}`,
     email: `${process.env.NEXT_PUBLIC_PROFILE_EMAIL}`,
