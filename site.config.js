6d2fdd93b23d3ddb5a5e2fffc501b0233331ba87
Doruk Taneli <45591689+DorukTaneli@users.noreply.github.com>
2024-01-08 22:26:00 +0100

    personalize site.config.js

--- site.config.js
+++ site.config.js
@@ -1,30 +1,30 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: "morethanmin",
-    image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
-    role: "frontend developer",
-    bio: "I develop everything using node.",
-    email: "morethanmin.dev@gmail.com",
-    linkedin: "morethanmin",
-    github: "morethanmin",
-    instagram: "",
+    name: "Doruk Taneli",
+    image: "/pp.jpg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
+    role: "Software Engineer",
+    bio: "I like ducks.",
+    email: "doruktaneli@gmail.com",
+    linkedin: "doruktaneli",
+    github: "DorukTaneli",
+    instagram: "doruktaneli",
   },
   projects: [
     {
-      name: `morethan-log`,
-      href: "https://github.com/morethanmin/morethan-log",
+      name: `projects`,
+      href: "https://github.com/DorukTaneli",
     },
   ],
   // blog setting (required)
   blog: {
-    title: "morethan-log",
-    description: "welcome to morethan-log!",
+    title: "Doruk Taneli",
+    description: "Welcome to my personal website!",
   },
 
   // CONFIG configration (required)
-  link: "https://morethan-log.vercel.app",
-  since: 2022, // If leave this empty, current year will be used.
+  link: "https://www.doruktaneli.com",
+  since: 2024, // If leave this empty, current year will be used.
   lang: "en-US", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
   ogImageGenerateURL: "https://og-image-korean.vercel.app", // The link to generate OG image, don't end with a slash
 
