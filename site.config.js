faf05807490b0e85af64c87577594332ed20dd76
Kohei-Katashima <kkohei0325@gmail.com>
2024-01-20 15:21:17 +0900

    自分用に編集

--- site.config.js
+++ site.config.js
@@ -1,31 +1,35 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: "morethanmin",
+    name: "kohek",
     image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
-    role: "frontend developer",
-    bio: "I develop everything using node.",
-    email: "morethanmin.dev@gmail.com",
-    linkedin: "morethanmin",
-    github: "morethanmin",
+    role: "フロント バックエンド インフラエンジニア",
+    bio: "Vue.js TypeScript AWS Lambdaを使用して開発しています。",
+    email: "setdiagram.info@gmail.com",
+    linkedin: "",
+    github: "kohek-senbei",
     instagram: "",
   },
   projects: [
+    // {
+    //   name: `notion-blog`,
+    //   href: "https://github.com/morethanmin/morethan-log",
+    // },
     {
-      name: `morethan-log`,
-      href: "https://github.com/morethanmin/morethan-log",
+      name: `セット図メーカー`,
+      href: "https://set-diagram.com",
     },
   ],
   // blog setting (required)
   blog: {
-    title: "morethan-log",
-    description: "welcome to morethan-log!",
+    title: "kohek-senbei-blog",
+    description: "welcome to kohek-senbei-blog!",
   },
 
   // CONFIG configration (required)
   link: "https://morethan-log.vercel.app",
-  since: 2022, // If leave this empty, current year will be used.
-  lang: "en-US", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
+  since: 2024, // If leave this empty, current year will be used.
+  lang: "ja-JP", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
   ogImageGenerateURL: "https://og-image-korean.vercel.app", // The link to generate OG image, don't end with a slash
 
   // notion configuration (required)
--- src/routes/Feed/SearchInput.tsx
+++ src/routes/Feed/SearchInput.tsx
@@ -13,7 +13,7 @@ const SearchInput: React.FC<Props> = ({ ...props }) => {
       <input
         className="mid"
         type="text"
-        placeholder="Search Keyword..."
+        placeholder="キーワード..."
         {...props}
       />
     </StyledWrapper>
