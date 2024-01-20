050618ffa7077776d6a0625a69827eb27975819b
Edcleidson de Souza Cardoso Junior <eddieprofessionalmail@gmail.com>
2024-01-19 22:06:19 -0300

    feature: adding language highlight imports to the root layout
    adding language highlight imports to the
    root layout and starting the highlight from useEffect,
    it wil make the prism enable the highlight in the
    code blocks

--- src/layouts/RootLayout/index.tsx
+++ src/layouts/RootLayout/index.tsx
@@ -1,10 +1,44 @@
-import React, { ReactNode } from "react"
+import React, { ReactNode, useEffect } from "react"
 import { ThemeProvider } from "./ThemeProvider"
 import useScheme from "src/hooks/useScheme"
 import Header from "./Header"
 import styled from "@emotion/styled"
 import Scripts from "src/layouts/RootLayout/Scripts"
 import useGtagEffect from "./useGtagEffect"
+import Prism from "prismjs/prism"
+import 'prismjs/components/prism-markup-templating.js'
+import 'prismjs/components/prism-markup.js'
+import 'prismjs/components/prism-bash.js'
+import 'prismjs/components/prism-c.js'
+import 'prismjs/components/prism-cpp.js'
+import 'prismjs/components/prism-csharp.js'
+import 'prismjs/components/prism-docker.js'
+import 'prismjs/components/prism-java.js'
+import 'prismjs/components/prism-js-templates.js'
+import 'prismjs/components/prism-coffeescript.js'
+import 'prismjs/components/prism-diff.js'
+import 'prismjs/components/prism-git.js'
+import 'prismjs/components/prism-go.js'
+import 'prismjs/components/prism-kotlin.js'
+import 'prismjs/components/prism-graphql.js'
+import 'prismjs/components/prism-handlebars.js'
+import 'prismjs/components/prism-less.js'
+import 'prismjs/components/prism-makefile.js'
+import 'prismjs/components/prism-markdown.js'
+import 'prismjs/components/prism-objectivec.js'
+import 'prismjs/components/prism-ocaml.js'
+import 'prismjs/components/prism-python.js'
+import 'prismjs/components/prism-reason.js'
+import 'prismjs/components/prism-rust.js'
+import 'prismjs/components/prism-sass.js'
+import 'prismjs/components/prism-scss.js'
+import 'prismjs/components/prism-solidity.js'
+import 'prismjs/components/prism-sql.js'
+import 'prismjs/components/prism-stylus.js'
+import 'prismjs/components/prism-swift.js'
+import 'prismjs/components/prism-wasm.js'
+import 'prismjs/components/prism-yaml.js'
+import "prismjs/components/prism-go.js"
 
 type Props = {
   children: ReactNode
@@ -13,6 +47,10 @@ type Props = {
 const RootLayout = ({ children }: Props) => {
   const [scheme] = useScheme()
   useGtagEffect()
+  useEffect(() => {
+    Prism.highlightAll();
+  }, []);
+
   return (
     <ThemeProvider scheme={scheme}>
       <Scripts />
