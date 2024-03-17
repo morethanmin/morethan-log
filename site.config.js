2d98a0f2beaf81acb71e3bc5c9cadabcbdb12294
kwakjoonhyung <84233813+kwakjoonhyung@users.noreply.github.com>
2024-03-17 23:02:15 +0900

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -18,8 +18,8 @@ const CONFIG = {
   ],
   // blog setting (required)
   blog: {
-    title: "morethan-log",
-    description: "welcome to morethan-log!",
+    title: "AI Study-log",
+    description: "welcome to AI Study-log!",
   },
 
   // CONFIG configration (required)
