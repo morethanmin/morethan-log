d5f8c0f987a50965a435942cb82a36eed890ac98
MarlonJerold <jeroldmarlon5@gamail.com>
2024-05-26 16:48:55 -0300

    add image patinho

--- site.config.js
+++ site.config.js
@@ -2,7 +2,7 @@ const CONFIG = {
   // profile setting (required)
   profile: {
     name: "patinho",
-    image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
+    image: "/avatar.png", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
     role: "backend developer",
     bio: "I develop everything using Java.",
     email: "jeroldmarlon5@gmail.com",
