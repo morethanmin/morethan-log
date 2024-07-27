a1c8f2d442c37fb7283653086c7f5b8eb9db9f41
Hakim Bouzourdaz <63076613+hbouzourdaz@users.noreply.github.com>
2024-07-28 00:45:46 +0100

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -54,9 +54,9 @@ const CONFIG = {
     },
   },
   utterances: {
-    enable: false,
+    enable: true,
     config: {
-      repo: process.env.NEXT_PUBLIC_UTTERANCES_REPO || "",
+      repo: process.env.NEXT_PUBLIC_UTTERANCES_REPO || "https://github.com/hbouzourdaz/hakim-tech",
       "issue-term": "og:title",
       label: "💬 Utterances",
     },
