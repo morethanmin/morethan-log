e44f06960d0935275e8362d646fc06b84796ccbc
Hakim Bouzourdaz <63076613+hbouzourdaz@users.noreply.github.com>
2024-07-27 21:57:08 +0100

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,25 +1,25 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: "morethanmin",
+    name: "hakim-tech",
     image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
-    role: "frontend developer",
-    bio: "I develop everything using node.",
-    email: "morethanmin.dev@gmail.com",
-    linkedin: "morethanmin",
-    github: "morethanmin",
-    instagram: "",
+    role: "Physics Teacher",
+    bio: "keep it simple.",
+    email: "hbouzourdaz@gmail.com",
+    linkedin: "hbouzourdaz",
+    github: "hbouzourdaz",
+    instagram: "h_bzr21",
   },
   projects: [
     {
-      name: `morethan-log`,
-      href: "https://github.com/morethanmin/morethan-log",
+      name: `hakim-tech`,
+      href: "https://github.com/hbouzourdaz/hakim-tech/",
     },
   ],
   // blog setting (required)
   blog: {
-    title: "morethan-log",
-    description: "welcome to morethan-log!",
+    title: "hakim-tech",
+    description: "welcome to hakim-tech!",
     scheme: "dark", // 'light' | 'dark' | 'system'
   },
 
@@ -54,7 +54,7 @@ const CONFIG = {
     },
   },
   utterances: {
-    enable: true,
+    enable: false,
     config: {
       repo: process.env.NEXT_PUBLIC_UTTERANCES_REPO || "",
       "issue-term": "og:title",
