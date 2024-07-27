d542e5791d8b4312d662dfed0a86fb5b78027fde
ParkAward <parksangjun1363@gmail.com>
2024-07-27 19:07:37 +0900

    feat: Add dynamic Mermaid rendering with theme support
    - Integrated memoization to cache and reuse Mermaid diagrams.
    - Applied animation for smooth transition when rendering Mermaid diagrams.
    - Added useMermaidEffect hook to initialize Mermaid with dark or default theme based on Scheme
    - Added CSS styling for Mermaid code blocks with theme-based background color.

--- src/routes/Detail/hooks/useMermaidEffect.ts
+++ src/routes/Detail/hooks/useMermaidEffect.ts
@@ -1,5 +1,8 @@
+import { useQuery, useQueryClient } from "@tanstack/react-query"
 import mermaid from "mermaid"
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
+import { queryKey } from "src/constants/queryKey"
+import useScheme from "src/hooks/useScheme"
 
 /**
  *  Wait for mermaid to be defined in the dom
@@ -24,26 +27,53 @@ const waitForMermaid = (interval = 100, timeout = 5000) => {
   })
 }
 const useMermaidEffect = () => {
+  const [memoMermaid, setMemoMermaid] = useState<Map<number, string>>(new Map())
+
+  const { data, isFetched } = useQuery({
+    queryKey: queryKey.scheme(),
+    enabled: false,
+  })
+
   useEffect(() => {
+    if (!isFetched) return
     mermaid.initialize({
       startOnLoad: true,
+      theme: (data as "dark" | "light") === "dark" ? "dark" : "default",
     })
+
     if (!document) return
+
     waitForMermaid()
       .then(async (elements) => {
-        const promises = Array.from(elements).map(async (element, i) => {
-          const svg = await mermaid.render(
-            "mermaid" + i,
-            element.textContent || ""
-          ).then(res => res.svg)
-          element.innerHTML = svg
-        })
+        const promises = Array.from(elements)
+          .filter((elements) => elements.tagName === "PRE")
+          .map(async (element, i) => {
+            if (memoMermaid.get(i) !== undefined) {
+              const svg = await mermaid
+                .render("mermaid" + i, memoMermaid.get(i) || "")
+                .then((res) => res.svg)
+              element.animate(
+                [
+                  { easing: "ease-in", opacity: 0 },
+                  { easing: "ease-out", opacity: 1 },
+                ],
+                { duration: 300, fill: "both" }
+              )
+              element.innerHTML = svg
+              return
+            }
+            const svg = await mermaid
+              .render("mermaid" + i, element.textContent || "")
+              .then((res) => res.svg)
+            setMemoMermaid(memoMermaid.set(i, element.textContent ?? ""))
+            element.innerHTML = svg
+          })
         await Promise.all(promises)
       })
       .catch((error) => {
         console.warn(error)
       })
-  }, [])
+  }, [data, isFetched])
 
   return
 }
