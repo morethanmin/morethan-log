eb5dfeda9a234366dae3ee72754221e57f81ae8a
chae-sy <67832005+chae-sy@users.noreply.github.com>
2024-08-03 23:07:25 +0900

    Update ProfileCard.tsx

--- src/routes/Feed/ProfileCard.tsx
+++ src/routes/Feed/ProfileCard.tsx
@@ -62,7 +62,7 @@ const StyledWrapper = styled.div`
       .name {
         font-size: 1.25rem;
         line-height: 1.75rem;
-        font-style: italic;
+        // font-style: italic;
         font-weight: 700;
       }
       .role {
