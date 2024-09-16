5da634444c971e20cb0b3dbfe3aa029e78bf482c
Yelissa <145866021+Yelissa188@users.noreply.github.com>
2024-09-16 23:13:43 +0800

    Update site.config.js
    before

--- site.config.js
+++ site.config.js
@@ -1,13 +1,13 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: "morethanmin",
+    name: "Yelissa",
     image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
-    role: "frontend developer",
-    bio: "I develop everything using node.",
-    email: "morethanmin.dev@gmail.com",
-    linkedin: "morethanmin",
-    github: "morethanmin",
+    role: "Graduate Student",
+    bio: "To post useful information and artcles from my collection.",
+    email: "solovled188@gmail.com",
+    linkedin: "",
+    github: "Yelissa",
     instagram: "",
   },
   projects: [
@@ -18,9 +18,9 @@ const CONFIG = {
   ],
   // blog setting (required)
   blog: {
-    title: "morethan-log",
-    description: "welcome to morethan-log!",
-    scheme: "dark", // 'light' | 'dark' | 'system'
+    title: "Yelissa Pages",
+    description: "Have a good day",
+    scheme: "system", // 'light' | 'dark' | 'system'
   },
 
   // CONFIG configration (required)
