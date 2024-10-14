ba62c831945ceddd755a0b8c4df86892b5c6e4f7
GAYOUNG <107148237+JUGGUM@users.noreply.github.com>
2024-10-14 09:05:49 +0900

    Update filterPosts.ts

--- src/libs/utils/notion/filterPosts.ts
+++ src/libs/utils/notion/filterPosts.ts
@@ -21,20 +21,19 @@ export function filterPosts(
   const { acceptStatus = ["Public"], acceptType = ["Post"] } = options
   const filteredPosts = posts
     // filter data
-    .filter((post) => {
-      const postDate = new Date(post?.date?.start_date || post.createdTime)
-      if (!post.title || !post.slug || postDate > tomorrow) return false
-      return true
-    })
+  .filter(post => {
+    const postDate = new Date(post?.date?.start_date || post.createdTime);
+    return post.title && post.slug && postDate <= tomorrow;
+  })
     // filter status
-    .filter((post) => {
-      const postStatus = post.status[0]
-      return acceptStatus.includes(postStatus)
-    })
+  .filter(post => {
+    const postStatus = post.status?.[0];  // status가 배열이고 값이 있는지 확인
+    return postStatus && acceptStatus.includes(postStatus);
+  })
     // filter type
-    .filter((post) => {
-      const postType = post.type[0]
-      return acceptType.includes(postType)
-    })
+  .filter(post => {
+    const postType = post.type?.[0];  // type이 배열이고 값이 있는지 확인
+    return postType && acceptType.includes(postType);
+  });
   return filteredPosts
 }
