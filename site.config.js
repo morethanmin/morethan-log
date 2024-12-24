f2e8fc316b026963e743f0f24739af1803003d1d
burnout909 <newhorizon2232@gmail.com>
2024-12-24 21:15:38 +0900

    blog_name_change

--- site.config.js
+++ site.config.js
@@ -1,11 +1,11 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: "morethanmin",
+    name: "min-log",
     image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
-    role: "frontend developer",
-    bio: "I develop everything using node.",
-    email: "morethanmin.dev@gmail.com",
+    role: "ML Engineer",
+    bio: "I interested in Medical AI Engineering.",
+    email: "znlwmdhkd1@gmail.com",
     linkedin: "morethanmin",
     github: "morethanmin",
     instagram: "",
