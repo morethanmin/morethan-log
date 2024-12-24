edea26bcb4c152f4cd0de139d109f9cba6378c31
halfcodz <jeongusrn@gmail.com>
2024-12-25 07:06:37 +0900

    Update site.config.js
    change to hyeon-log

--- site.config.js
+++ site.config.js
@@ -1,24 +1,24 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: "morethanmin",
+    name: "halfcodz",
     image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
-    role: "frontend developer",
-    bio: "I develop everything using node.",
-    email: "morethanmin.dev@gmail.com",
-    linkedin: "morethanmin",
-    github: "morethanmin",
-    instagram: "",
+    role: "to be code master",
+    bio: "머리는 차갑게 가슴은 뜨겁게",
+    email: "jeongusrn@gmail.com",
+    linkedin: "halfcodz",
+    github: "halfcodz",
+    instagram: "https://www.instagram.com/01hg_jeon/",
   },
   projects: [
     {
-      name: `morethan-log`,
-      href: "https://github.com/morethanmin/morethan-log",
+      name: `hyeon-log`,
+      href: "https://github.com/halfcodz",
     },
   ],
   // blog setting (required)
   blog: {
-    title: "morethan-log",
+    title: "hyeon-log",
     description: "welcome to morethan-log!",
     scheme: "dark", // 'light' | 'dark' | 'system'
   },
