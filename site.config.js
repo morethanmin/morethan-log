3069870600ca8762a80fa559cad870aee977b411
SONGJUNHEE <111993984+0cars0903@users.noreply.github.com>
2025-02-23 14:00:24 +0900

    Update site.config.js
    repo: "0cars0903/utteranc"

--- site.config.js
+++ site.config.js
@@ -56,7 +56,7 @@ const CONFIG = {
   utterances: {
     enable: true,
     config: {
-      repo: process.env.NEXT_PUBLIC_UTTERANCES_REPO || "",
+      repo: "0cars0903/utteranc",
       "issue-term": "og:title",
       label: "💬 Utterances",
     },
