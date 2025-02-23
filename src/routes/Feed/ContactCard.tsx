097838eb7aa8640b63fd729bd4bf69c3dbe88471
SONGJUNHEE <111993984+0cars0903@users.noreply.github.com>
2025-02-23 14:32:00 +0900

    Update ContactCard.tsx

--- src/routes/Feed/ContactCard.tsx
+++ src/routes/Feed/ContactCard.tsx
@@ -18,7 +18,7 @@ const ContactCard: React.FC = () => {
       <StyledWrapper>
         {CONFIG.profile.github && (
           <a
-            href={`https://github.com/0cars0903`}
+            href={`https://github.com/${CONFIG.profile.github}`}
             rel="noreferrer"
             target="_blank"
           >
@@ -38,7 +38,7 @@ const ContactCard: React.FC = () => {
         )}
         {CONFIG.profile.email && (
           <a
-            href={`mailto:thdwnsgml521@gmail.com`}
+            href={`mailto:${CONFIG.profile.email}`}
             rel="noreferrer"
             target="_blank"
             css={{ overflow: "hidden" }}
@@ -49,7 +49,7 @@ const ContactCard: React.FC = () => {
         )}
         {CONFIG.profile.linkedin && (
           <a
-            href={`https://www.linkedin.com/in/0cars0903`}
+            href={`https://www.linkedin.com/in/${CONFIG.profile.linkedin}`}
             rel="noreferrer"
             target="_blank"
           >
