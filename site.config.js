625a77f1c07d103229a8973c767e85983fe1d6e6
yujin <127318207+sophia22001@users.noreply.github.com>
2025-04-09 00:55:57 +0900

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -69,7 +69,7 @@ const CONFIG = {
     },
   },
   isProd: process.env.VERCEL_ENV === "production", // distinguish between development and production environment (ref: https://vercel.com/docs/environment-variables#system-environment-variables)
-  revalidateTime: 21600 * 7, // revalidate time for [slug], index
+  revalidateTime: 60, // revalidate time for [slug], index
 }
 
 module.exports = { CONFIG }
