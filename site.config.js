5b14257c09c7979efeae3a16ea82b1b1d23586de
pawntoqueen <gayemsuner@gmail.com>
2025-06-06 19:03:17 +0300

    index file changed

--- site.config.js
+++ site.config.js
@@ -1,31 +1,30 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: "morethanmin",
-    image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
-    role: "frontend developer",
-    bio: "I develop everything using node.",
-    email: "morethanmin.dev@gmail.com",
-    linkedin: "morethanmin",
-    github: "morethanmin",
+    name: "Gaye Suner",
+    image: "/image.png", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
+    role: "Cloud Engineer",
+    bio: "virtualization, cloud, automation, orchestration, devops",
+    email: "gayemsuner@gmail.com",
+    linkedin: "",
+    github: "",
     instagram: "",
   },
   projects: [
     {
-      name: `morethan-log`,
-      href: "https://github.com/morethanmin/morethan-log",
+      name: `Private Cloud Blogs`,
     },
   ],
   // blog setting (required)
   blog: {
-    title: "morethan-log",
-    description: "welcome to morethan-log!",
+    title: "Private Cloud Blogs",
+    description: "Burada Automation ve Orchestration hakkinda bilgi paylasıyorum.",
     scheme: "dark", // 'light' | 'dark' | 'system'
   },
 
   // CONFIG configration (required)
-  link: "https://morethan-log.vercel.app",
-  since: 2022, // If leave this empty, current year will be used.
+  link: "www.gayesuner.com",
+  since: 2024, // If leave this empty, current year will be used.
   lang: "en-US", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
   ogImageGenerateURL: "https://og-image-korean.vercel.app", // The link to generate OG image, don't end with a slash
 
