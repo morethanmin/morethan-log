5ca582c4144f98fb4519e3283f799322494d1dec
KingPlatypus <dlduswp0408@gmail.com>
2025-06-07 17:55:44 +0900

    Update index.tsx

--- src/routes/Detail/PageDetail/index.tsx
+++ src/routes/Detail/PageDetail/index.tsx
@@ -1,6 +1,6 @@
 import styled from "@emotion/styled"
 import React from "react"
-import NotionRenderer from "../../components/NotionRenderer"
+import NotionRenderer from "../components/NotionRenderer"
 // type Props = {}
 
 // const PageDetail: React.FC<Props> = () => {
