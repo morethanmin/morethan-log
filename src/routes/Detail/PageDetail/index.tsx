581097c7c68525b75a9209e7e3553b150bf47766
KingPlatypus <dlduswp0408@gmail.com>
2025-06-07 17:50:49 +0900

    ak

--- src/routes/Detail/PageDetail/index.tsx
+++ src/routes/Detail/PageDetail/index.tsx
@@ -1,16 +1,33 @@
-import React from "react"
 import styled from "@emotion/styled"
-import NotionRenderer from "../components/NotionRenderer"
-import usePostQuery from "src/hooks/usePostQuery"
-type Props = {}
+import React from "react"
+import NotionRenderer from "../../components/NotionRenderer"
+// type Props = {}
 
-const PageDetail: React.FC<Props> = () => {
-  const data = usePostQuery()
+// const PageDetail: React.FC<Props> = () => {
+//   const data = usePostQuery()
 
-  if (!data) return null
+//   if (!data) return null
+//   return (
+//     <StyledWrapper>
+//       <NotionRenderer recordMap={data.recordMap} />
+//     </StyledWrapper>
+//   )
+// }
+
+// export default PageDetail
+
+// const StyledWrapper = styled.div`
+//   margin: 0 auto;
+//   max-width: 56rem;
+// `
+type Props = {
+  recordMap: any
+}
+
+const PageDetail: React.FC<Props> = ({ recordMap }) => {
   return (
     <StyledWrapper>
-      <NotionRenderer recordMap={data.recordMap} />
+      <NotionRenderer recordMap={recordMap} />
     </StyledWrapper>
   )
 }
--- src/routes/Detail/index.tsx
+++ src/routes/Detail/index.tsx
@@ -1,59 +1,20 @@
-// import useMermaidEffect from "./hooks/useMermaidEffect"
-// import PostDetail from "./PostDetail"
-// import PageDetail from "./PageDetail"
-// import styled from "@emotion/styled"
-// import usePostQuery from "src/hooks/usePostQuery"
-
-// type Props = {}
-
-// const Detail: React.FC<Props> = () => {
-//   const data = usePostQuery()
-//   useMermaidEffect()
-
-//   if (!data) return null
-//   return (
-//     <StyledWrapper data-type={data.type}>
-//       {data.type[0] === "Page" && <PageDetail />}
-//       {data.type[0] !== "Page" && <PostDetail />}
-//     </StyledWrapper>
-//   )
-// }
-
-// export default Detail
-
-// const StyledWrapper = styled.div`
-//   padding: 2rem 0;
-
-//   &[data-type="Paper"] {
-//     padding: 40px 0;
-//   }
-//   /** Reference: https://github.com/chriskempson/tomorrow-theme **/
-//   code[class*="language-mermaid"],
-//   pre[class*="language-mermaid"] {
-//     background-color: ${({ theme }) => theme.colors.gray5};
-//   }
-// `
-
 import styled from "@emotion/styled"
 import usePostQuery from "src/hooks/usePostQuery"
 import useMermaidEffect from "./hooks/useMermaidEffect"
 import PageDetail from "./PageDetail"
 import PostDetail from "./PostDetail"
 
-type Props = {
-  recordMap: any
-}
+type Props = {}
 
-const Detail: React.FC<Props> = ({ recordMap }) => {
+const Detail: React.FC<Props> = () => {
   const data = usePostQuery()
   useMermaidEffect()
 
   if (!data) return null
-
   return (
     <StyledWrapper data-type={data.type}>
-      {data.type[0] === "Page" && <PageDetail recordMap={recordMap} />}
-      {data.type[0] !== "Page" && <PostDetail recordMap={recordMap} />}
+      {data.type[0] === "Page" && <PageDetail />}
+      {data.type[0] !== "Page" && <PostDetail />}
     </StyledWrapper>
   )
 }
@@ -66,7 +27,6 @@ const StyledWrapper = styled.div`
   &[data-type="Paper"] {
     padding: 40px 0;
   }
-
   /** Reference: https://github.com/chriskempson/tomorrow-theme **/
   code[class*="language-mermaid"],
   pre[class*="language-mermaid"] {
