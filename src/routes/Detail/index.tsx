3f3ed233166f549d6424d470b885a27acff52d5f
KingPlatypus <dlduswp0408@gmail.com>
2025-06-07 17:54:12 +0900

    Update index.tsx

--- src/routes/Detail/index.tsx
+++ src/routes/Detail/index.tsx
@@ -1,20 +1,47 @@
 import styled from "@emotion/styled"
-import usePostQuery from "src/hooks/usePostQuery"
-import useMermaidEffect from "./hooks/useMermaidEffect"
 import PageDetail from "./PageDetail"
 import PostDetail from "./PostDetail"
 
-type Props = {}
+// type Props = {}
 
-const Detail: React.FC<Props> = () => {
-  const data = usePostQuery()
-  useMermaidEffect()
+// const Detail: React.FC<Props> = () => {
+//   const data = usePostQuery()
+//   useMermaidEffect()
 
-  if (!data) return null
+//   if (!data) return null
+//   return (
+//     <StyledWrapper data-type={data.type}>
+//       {data.type[0] === "Page" && <PageDetail />}
+//       {data.type[0] !== "Page" && <PostDetail />}
+//     </StyledWrapper>
+//   )
+// }
+
+// export default Detail
+
+// const StyledWrapper = styled.div`
+//   padding: 2rem 0;
+
+//   &[data-type="Paper"] {
+//     padding: 40px 0;
+//   }
+//   /** Reference: https://github.com/chriskempson/tomorrow-theme **/
+//   code[class*="language-mermaid"],
+//   pre[class*="language-mermaid"] {
+//     background-color: ${({ theme }) => theme.colors.gray5};
+//   }
+// `
+type Props = {
+  recordMap: any
+}
+
+const Detail: React.FC<Props> = ({ recordMap }) => {
+  const data = recordMap.block[Object.keys(recordMap.block)[0]].value
+
   return (
     <StyledWrapper data-type={data.type}>
-      {data.type[0] === "Page" && <PageDetail />}
-      {data.type[0] !== "Page" && <PostDetail />}
+      {data.type[0] === "Page" && <PageDetail recordMap={recordMap} />}
+      {data.type[0] !== "Page" && <PostDetail recordMap={recordMap} />}
     </StyledWrapper>
   )
 }
@@ -22,14 +49,6 @@ const Detail: React.FC<Props> = () => {
 export default Detail
 
 const StyledWrapper = styled.div`
-  padding: 2rem 0;
-
-  &[data-type="Paper"] {
-    padding: 40px 0;
-  }
-  /** Reference: https://github.com/chriskempson/tomorrow-theme **/
-  code[class*="language-mermaid"],
-  pre[class*="language-mermaid"] {
-    background-color: ${({ theme }) => theme.colors.gray5};
-  }
+  margin: 0 auto;
+  max-width: 56rem;
 `
