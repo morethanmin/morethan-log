ab2dc7f5a41d18ba4c42d17c5850e7aed1e32b2e
KingPlatypus <dlduswp0408@gmail.com>
2025-06-07 19:02:16 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,27 @@
-module.exports = {
+// module.exports = {
+//   images: {
+//     domains: ['www.notion.so', 'lh5.googleusercontent.com', 's3-us-west-2.amazonaws.com'],
+//   },
+// }
+// next.config.js
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
   images: {
     domains: ['www.notion.so', 'lh5.googleusercontent.com', 's3-us-west-2.amazonaws.com'],
   },
-}
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        fs: false,
+        net: false,
+        tls: false,
+        dns: false,
+      };
+    }
+    return config;
+  },
+};
+
+module.exports = nextConfig;
