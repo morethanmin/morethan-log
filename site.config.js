5fd1d44ff5e7bcc5dae5805465ad554b84a517dd
Ha Jun Yoo <58240677+HaJunYoo@users.noreply.github.com>
2025-06-10 18:08:06 +0900

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -2,7 +2,7 @@ const CONFIG = {
   // profile setting (required)
   profile: {
     name: "HaJunYoo",
-    image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
+    image: "/notion-avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
     role: "software developer",
     bio: "I like making software.",
     email: "hajuny129@gmail.com",
