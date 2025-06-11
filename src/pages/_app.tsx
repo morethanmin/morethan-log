5edc263741dc0e0aeaf3fd69ed1f84f6eba5f5fe
HaJunYoo <hajuny129@gmail.com>
2025-06-11 21:51:07 +0900

    feat: progress bar for page loading & page scroll

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,16 +2,60 @@ import { AppPropsWithLayout } from "../types"
 import { Hydrate, QueryClientProvider } from "@tanstack/react-query"
 import { RootLayout } from "src/layouts"
 import { queryClient } from "src/libs/react-query"
+import NProgress from "nprogress"
+import "nprogress/nprogress.css"
+import { useEffect } from "react"
+import { useRouter } from "next/router"
+import { Global, css } from "@emotion/react"
 
 function App({ Component, pageProps }: AppPropsWithLayout) {
   const getLayout = Component.getLayout || ((page) => page)
+  const router = useRouter()
+
+  useEffect(() => {
+    const handleStart = () => NProgress.start()
+    const handleStop = () => NProgress.done()
+    router.events.on("routeChangeStart", handleStart)
+    router.events.on("routeChangeComplete", handleStop)
+    router.events.on("routeChangeError", handleStop)
+    return () => {
+      router.events.off("routeChangeStart", handleStart)
+      router.events.off("routeChangeComplete", handleStop)
+      router.events.off("routeChangeError", handleStop)
+    }
+  }, [router])
 
   return (
-    <QueryClientProvider client={queryClient}>
-      <Hydrate state={pageProps.dehydratedState}>
-        <RootLayout>{getLayout(<Component {...pageProps} />)}</RootLayout>
-      </Hydrate>
-    </QueryClientProvider>
+    <>
+      <Global
+        styles={css`
+          /* nprogress 커스텀 */
+          #nprogress {
+            pointer-events: none;
+          }
+          #nprogress .bar {
+            background: #6366f1;
+            position: fixed;
+            z-index: 1031;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 3px;
+          }
+          #nprogress .peg {
+            display: none;
+          }
+          #nprogress .spinner {
+            display: none;
+          }
+        `}
+      />
+      <QueryClientProvider client={queryClient}>
+        <Hydrate state={pageProps.dehydratedState}>
+          <RootLayout>{getLayout(<Component {...pageProps} />)}</RootLayout>
+        </Hydrate>
+      </QueryClientProvider>
+    </>
   )
 }
 
--- src/routes/Detail/PostDetail/index.tsx
+++ src/routes/Detail/PostDetail/index.tsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import PostHeader from "./PostHeader"
 import Footer from "./PostFooter"
 import CommentBox from "./CommentBox"
@@ -6,6 +6,41 @@ import Category from "src/components/Category"
 import styled from "@emotion/styled"
 import NotionRenderer from "../components/NotionRenderer"
 import usePostQuery from "src/hooks/usePostQuery"
+
+// 스크롤 프로그레스바 컴포넌트
+const ScrollProgressBar = () => {
+  const [progress, setProgress] = useState(0)
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollTop = window.scrollY
+      const docHeight = document.documentElement.scrollHeight - window.innerHeight
+      const percent = docHeight > 0 ? (scrollTop / docHeight) * 100 : 0
+      setProgress(percent)
+    }
+    window.addEventListener("scroll", handleScroll)
+    return () => window.removeEventListener("scroll", handleScroll)
+  }, [])
+  return (
+    <ProgressBarWrapper>
+      <ProgressBarInner style={{ width: `${progress}%` }} />
+    </ProgressBarWrapper>
+  )
+}
+
+const ProgressBarWrapper = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 4px;
+  background: transparent;
+  z-index: 2000;
+`
+const ProgressBarInner = styled.div`
+  height: 100%;
+  background: #6366f1;
+  transition: width 0.2s;
+`
 
 type Props = {}
 
@@ -18,6 +53,7 @@ const PostDetail: React.FC<Props> = () => {
 
   return (
     <StyledWrapper>
+      <ScrollProgressBar />
       <article>
         {category && (
           <div css={{ marginBottom: "0.5rem" }}>
