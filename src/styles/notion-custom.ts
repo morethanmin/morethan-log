0379febdd46f36f1b26939a237ecf51288b168da
HaJunYoo <hajuny129@gmail.com>
2025-06-12 23:28:19 +0900

    feat: 구분선 아래, 위 마진

--- src/styles/notion-custom.ts
+++ src/styles/notion-custom.ts
@@ -58,16 +58,14 @@ export const notionCustomStyles = css`
   .notion-list > ol,
   .notion-list > ul {
     margin-left: 0 !important;
-    padding-left: 0 !important;
-    list-style-position: inside !important;
+    padding-left: 1.3em !important;
   }
-  /* 하위(subset) 리스트는 들여쓰기 적용 */
-  .notion-list ol ol,
-  .notion-list ul ul,
-  .notion-list ol ul,
-  .notion-list ul ol {
-    margin-left: 0 !important;
-    padding-left: 0.7em !important;
-    list-style-position: inside !important;
+  /* 첫 번째 리스트 항목만 왼쪽 패딩 제거 */
+  .notion-list > ol > li:first-child,
+  .notion-list > ul > li:first-child {
+    padding-left: 0 !important;
+  }
+  .notion-hr {
+    margin: 2em 0 !important;
   }
 `;
