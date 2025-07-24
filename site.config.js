9fb423643ff9fc254bb954e1e8a138a12ea39900
hajun.yoo <hajun.yoo@hajunyooui-MacBookPro.local>
2025-07-24 11:26:19 +0900

    feat: config link 변경

--- site.config.js
+++ site.config.js
@@ -24,7 +24,7 @@ const CONFIG = {
   },
 
   // CONFIG configration (required)
-  link: "https://yukis-dev-log.vercel.app",
+  link: "https://www.yukis-dev-log.site",
   since: 2024, // If leave this empty, current year will be used.
   lang: "en-US", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
   ogImageGenerateURL: "https://og-image-korean.vercel.app", // The link to generate OG image, don't end with a slash
