fcbde522d467c2a9bd81064e60b5889fbdd482f4
seoseuo <seoseuo@naver.com>
2025-08-17 14:01:01 +0900

    Bugfix : 댓글창 오류 수정

--- src/routes/Detail/PostDetail/CommentBox/index.tsx
+++ src/routes/Detail/PostDetail/CommentBox/index.tsx
@@ -1,45 +1,31 @@
 import { TPost } from "src/types"
-import { CONFIG } from "site.config"
-import dynamic from "next/dynamic"
-import React, { useEffect, useRef } from "react";
-
-const UtterancesComponent = dynamic(
-  () => {
-    return import("./Utterances")
-  },
-  { ssr: false }
-)
-const CusdisComponent = dynamic(
-  () => {
-    return import("./Cusdis")
-  },
-  { ssr: false }
-)
+import React, { useEffect, useRef } from "react"
 
 type Props = {
   data: TPost
 }
 
-const CommentBox: React.FC = () => {
-  const containerRef = useRef<HTMLDivElement>(null);
+const CommentBox: React.FC<Props> = ({ data }) => {
+  const containerRef = useRef<HTMLDivElement>(null)
 
   useEffect(() => {
-    if (!containerRef.current) return;
+    if (!containerRef.current) return
+    // 이미 스크립트가 추가되어 있으면 중복 방지
+    if (containerRef.current.childElementCount > 0) return
 
-    const script = document.createElement("script");
-    script.src = "https://utteranc.es/client.js";
-    script.async = true;
-    script.crossOrigin = "anonymous";
-    script.setAttribute("repo", "seoseuo/morethan-log");
-    script.setAttribute("issue-term", "pathname");
-    script.setAttribute("label", "Comment");
-    script.setAttribute("theme", "github-light");
+    const script = document.createElement("script")
+    script.src = "https://utteranc.es/client.js"
+    script.async = true
+    script.crossOrigin = "anonymous"
+    script.setAttribute("repo", "seoseuo/morethan-log")
+    script.setAttribute("issue-term", "pathname")
+    script.setAttribute("label", "Comment")
+    script.setAttribute("theme", "github-light")
 
-    containerRef.current.appendChild(script);
-  }, []);
+    containerRef.current.appendChild(script)
+  }, [])
 
-  return <div ref={containerRef}></div>;
-};
-
+  return <div ref={containerRef}></div>
+}
 
 export default CommentBox
