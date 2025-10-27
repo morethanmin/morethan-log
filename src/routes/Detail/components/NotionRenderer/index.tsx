59d4b5df3fc9157b01d7f4ccdd7ed42e52420f03
KingPlatypus <dlduswp0408@gmail.com>
2025-10-27 18:48:57 +0900

    Update index.tsx

--- src/routes/Detail/components/NotionRenderer/index.tsx
+++ src/routes/Detail/components/NotionRenderer/index.tsx
@@ -1,20 +1,13 @@
+import styled from "@emotion/styled"
+import "katex/dist/katex.min.css"
 import dynamic from "next/dynamic"
 import Image from "next/image"
 import Link from "next/link"
-import { ExtendedRecordMap } from "notion-types"
-import useScheme from "src/hooks/useScheme"
-
-// core styles shared by all of react-notion-x (required)
-import "react-notion-x/src/styles.css"
-
-// used for code syntax highlighting (optional)
 import "prismjs/themes/prism-tomorrow.css"
-
-// used for rendering equations (optional)
-
-import "katex/dist/katex.min.css"
 import { FC } from "react"
-import styled from "@emotion/styled"
+import type { ExtendedRecordMap } from "react-notion-x"
+import "react-notion-x/src/styles.css"
+import useScheme from "src/hooks/useScheme"
 
 const _NotionRenderer = dynamic(
   () => import("react-notion-x").then((m) => m.NotionRenderer),
@@ -22,28 +15,25 @@ const _NotionRenderer = dynamic(
 )
 
 const Code = dynamic(() =>
-  import("react-notion-x/build/third-party/code").then(async (m) => m.Code)
+  import("react-notion-x/build/third-party/code").then((m) => m.Code)
 )
 
 const Collection = dynamic(() =>
-  import("react-notion-x/build/third-party/collection").then(
-    (m) => m.Collection
-  )
+  import("react-notion-x/build/third-party/collection").then((m) => m.Collection)
 )
+
 const Equation = dynamic(() =>
   import("react-notion-x/build/third-party/equation").then((m) => m.Equation)
 )
+
 const Pdf = dynamic(
   () => import("react-notion-x/build/third-party/pdf").then((m) => m.Pdf),
-  {
-    ssr: false,
-  }
+  { ssr: false }
 )
+
 const Modal = dynamic(
   () => import("react-notion-x/build/third-party/modal").then((m) => m.Modal),
-  {
-    ssr: false,
-  }
+  { ssr: false }
 )
 
 const mapPageUrl = (id: string) => {
@@ -68,7 +58,7 @@ const NotionRenderer: FC<Props> = ({ recordMap }) => {
           Modal,
           Pdf,
           nextImage: Image,
-          nextLink: Link,
+          nextLink: Link
         }}
         mapPageUrl={mapPageUrl}
       />
@@ -79,7 +69,6 @@ const NotionRenderer: FC<Props> = ({ recordMap }) => {
 export default NotionRenderer
 
 const StyledWrapper = styled.div`
-  /* // TODO: why render? */
   .notion-collection-page-properties {
     display: none !important;
   }
