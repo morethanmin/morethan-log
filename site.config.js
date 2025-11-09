62d3933debd7a3c15b630518baf8085c0b44246c
Byeoldomi <byeoldom@gmail.com>
2025-11-09 20:43:55 +0900

    Update avatar image path in site.config.js

--- site.config.js
+++ site.config.js
@@ -2,7 +2,7 @@ const CONFIG = {
   // profile setting (required)
   profile: {
     name: "morethanmin",
-    image: "public/avasta.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
+    image: "/avasta.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
     role: "frontend developer",
     bio: "I develop everything using node.",
     email: "morethanmin.dev@gmail.com",
