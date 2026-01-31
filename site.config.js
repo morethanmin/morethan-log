e301fb596de5df28567bb124db5eaca7fc0a5221
juyongSong <frenz104@gmail.com>
2026-02-01 01:13:19 +0900

    Update project name and blog settings in config

--- site.config.js
+++ site.config.js
@@ -12,20 +12,20 @@ const CONFIG = {
   },
   projects: [
     {
-      name: `morethan-log`,
-      href: "https://github.com/morethanmin/morethan-log",
+      name: `MyReadings`,
+      href: "https://github.com/juyongSong/MyReading",
     },
   ],
   // blog setting (required)
   blog: {
     title: "my reading",
     description: "Read together!",
-    scheme: "dark", // 'light' | 'dark' | 'system'
+    scheme: "light", // 'light' | 'dark' | 'system'
   },
 
   // CONFIG configration (required)
   link: "https://morethan-log.vercel.app",
-  since: 2022, // If leave this empty, current year will be used.
+  since: 2026, // If leave this empty, current year will be used.
   lang: "en-US", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
   ogImageGenerateURL: "https://og-image-korean.vercel.app", // The link to generate OG image, don't end with a slash
 
